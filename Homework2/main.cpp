207f7e83097fb4c32d260f29152e0d42d68e8fd4
Edmund <ninjataco127@gmail.com>
2021-04-23 11:30:58 -0700

    Initial Commit

---
+++ Homework2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework2
+//
+//  Created by Edmund Zhi on 4/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
