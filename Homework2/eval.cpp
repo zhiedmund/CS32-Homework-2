a14fbf4659fe22ac26215a02c0a3497dac927998
Edmund <ninjataco127@gmail.com>
2021-04-27 18:53:26 -0700

    second commit

--- Homework2/eval.cpp
+++ Homework2/eval.cpp
@@ -62,6 +62,9 @@ int evaluate(string infix, string& postfix, bool& result) {
                     converted += toInsert;
                     operatorStack.pop();
                 }
+                if (operatorStack.empty()) {
+                    return 1;
+                }
                 operatorStack.pop(); // removes the '('
                 closedCounter++;
                 break;
@@ -94,6 +97,7 @@ int evaluate(string infix, string& postfix, bool& result) {
         }
         prev = ch;
     }
+   
     // postfix expression must end with closed parenthesis or operand
     if (prev != ')' && prev != 'T' && prev != 'F') {
         return 1;
@@ -114,6 +118,7 @@ int evaluate(string infix, string& postfix, bool& result) {
 
     // converted is now finished, apply converted to postfix
     postfix = converted;
+    
     // Evaluate
     //Initialize the operand stack to empty
     stack<bool> operandStack;
@@ -135,7 +140,7 @@ int evaluate(string infix, string& postfix, bool& result) {
             // pop the stack
             operandStack.pop();
             if (ch == '!') { // ch is a unary operator
-                operandStack.push(operand2);
+                operandStack.push(!operand2);
             }
             else { // ch is a binary operator
                 // set operand1 to the top of the operand stack
@@ -165,7 +170,8 @@ int evaluate(string infix, string& postfix, bool& result) {
     return 0;
 }
 
-int main() {
+int main()
+{
     string pf;
     bool answer;
     assert(evaluate("T^ F", pf, answer) == 0  &&  pf == "TF^"  &&  answer);
@@ -179,9 +185,125 @@ int main() {
     assert(evaluate("(T&(F^F)", pf, answer) == 1);
     assert(evaluate("T|F", pf, answer) == 1);
     assert(evaluate("", pf, answer) == 1);
-    assert(evaluate("F  ^  !F & (T&F) ", pf, answer) == 0 &&  pf == "FF!TF&&^"  &&  !answer);
+    assert(evaluate("F  ^  !F & (T&F) ", pf, answer) == 0
+           &&  pf == "FF!TF&&^"  &&  !answer);
     assert(evaluate(" F  ", pf, answer) == 0 &&  pf == "F"  &&  !answer);
     assert(evaluate("((T))", pf, answer) == 0 &&  pf == "T"  &&  answer);
-    cout << "Passed all tests" << endl;
+    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer == true);
+    assert(evaluate("(F)", pf, answer) == 0 && pf == "F" && answer == false);
+    assert(evaluate("T ^(F)", pf, answer) == 0 && pf == "TF^" && answer == true);
+    assert(evaluate("T ^ !F", pf, answer) == 0 && pf == "TF!^" && answer == false);
+    assert(evaluate("!(T&!!  !!F)", pf, answer) == 0 && pf == "TF!!!!&!" && answer == true);
+    assert(evaluate("!T&F ", pf, answer) == 0 && pf == "T!F&" && answer == false);
+    assert(evaluate("T^F&F", pf, answer) == 0 && pf == "TFF&^" && answer == true);
+    assert(evaluate("T&! (F^T&T^F  )^!!!(F&T&F)", pf, answer) == 0 && pf == "TFTT&^F^!&FT&F&!!!^" && answer == true);
+    assert(evaluate("TS DF:L", pf, answer) == 1);
+    assert(evaluate("( F^T&!  F)", pf, answer) == 0 && pf == "FTF!&^" && answer == true);
+    assert(evaluate("(( F))&((! T))", pf, answer) == 0 && pf == "FT!&" && answer == false);
+    assert(evaluate("T^!F&T^T^(!F&T^!!T&F)", pf, answer) == 0 && answer == false);
+    assert(evaluate("!!(F^T&F)", pf, answer) == 0 && answer == false);
+    assert(evaluate("F^!T&T^!F", pf, answer) == 0 && answer == true);
+    assert(evaluate("T&(F^!T&  T^!F)&!!(F^T& !F)", pf, answer) == 0 && answer == true);
+    assert(evaluate("T^ F", pf, answer) == 0 && pf == "TF^"  &&  answer);
+    assert(evaluate("T^", pf, answer) == 1);
+    assert(evaluate("F F", pf, answer) == 1);
+    assert(evaluate("TF", pf, answer) == 1);
+    assert(evaluate("()", pf, answer) == 1);
+    assert(evaluate("()T", pf, answer) == 1);
+    assert(evaluate("T(F^T)", pf, answer) == 1);
+    assert(evaluate("T(&T)", pf, answer) == 1);
+    assert(evaluate("(T&(F^F)", pf, answer) == 1);
+    assert(evaluate("T|F", pf, answer) == 1);
+    assert(evaluate("T&()F", pf, answer) == 1);
+    assert(evaluate("T(&)F", pf, answer) == 1);
+    assert(evaluate("T&F^T()", pf, answer) == 1);
+    assert(evaluate("T&(F&T))&((T&T)", pf, answer) == 1);
+    assert(evaluate("((T))(F)", pf, answer) == 1);
+    assert(evaluate("F  ^  !F & (T&F) ", pf, answer) == 0
+           && pf == "FF!TF&&^" && !answer);
+    assert(evaluate(" F  ", pf, answer) == 0 && pf == "F" && !answer);
+    assert(evaluate("((((T))))", pf, answer) == 0 && pf == "T"  &&  answer);
+    assert(evaluate("(((T^F)()))", pf, answer) == 1);
+    assert(evaluate("T^ F", pf, answer) == 0 && pf == "TF^" && answer);
+    assert(evaluate("T^", pf, answer) == 1);
+    assert(evaluate("F F", pf, answer) == 1);
+    assert(evaluate("TF", pf, answer) == 1);
+    assert(evaluate("()", pf, answer) == 1);
+    assert(evaluate("()T", pf, answer) == 1);
+    assert(evaluate("T(F^T)", pf, answer) == 1);
+    assert(evaluate("T(&T)", pf, answer) == 1);
+    assert(evaluate("(T&(F^F)", pf, answer) == 1);
+    assert(evaluate("T&(F^F))", pf, answer) == 1);
+    assert(evaluate("T&(F^F)", pf, answer) == 0 && !answer && pf == "TFF^&");
+    assert(evaluate("     T   &     (    F   ^   F   )   ", pf, answer) == 0 && !answer && pf == "TFF^&");
+    assert(evaluate("T&(F^(T^(F&T)))", pf, answer) == 0 && answer && pf == "TFTFT&^^&");
+    assert(evaluate("!T&(F^(T^(F&T)))", pf, answer) == 0 && !answer && pf == "T!FTFT&^^&");
+    assert(evaluate("!T&!(T^F)", pf, answer) == 0 && !answer && pf == "T!TF^!&");
+    assert(evaluate("((T))", pf, answer) == 0 && answer && pf == "T");
+    assert(evaluate("((F))", pf, answer) == 0 && !answer && pf == "F");
+    assert(evaluate("(!)", pf, answer) == 1);
+    assert(evaluate("(^)", pf, answer) == 1);
+    assert(evaluate("(&)", pf, answer) == 1);
+    assert(evaluate("(T!)", pf, answer) == 1);
+    assert(evaluate("(T^)", pf, answer) == 1);
+    assert(evaluate("(T&)", pf, answer) == 1);
+    assert(evaluate("(&T)", pf, answer) == 1);
+    assert(evaluate("(^T)", pf, answer) == 1);
+    assert(evaluate("((T)", pf, answer) == 1);
+    assert(evaluate("(T))", pf, answer) == 1);
+    assert(evaluate("T|F", pf, answer) == 1);
+    assert(evaluate("", pf, answer) == 1);
+    assert(evaluate("    ", pf, answer) == 1);
+    assert(evaluate("!", pf, answer) == 1);
+    assert(evaluate("^", pf, answer) == 1);
+    assert(evaluate("&", pf, answer) == 1);
+    assert(evaluate("TF", pf, answer) == 1);
+    assert(evaluate("FT", pf, answer) == 1);
+    assert(evaluate("F  ^  !F & (T&F) ", pf, answer) == 0 && pf == "FF!TF&&^" && !answer);
+    assert(evaluate(" F  ", pf, answer) == 0 && pf == "F" && !answer);
+    assert(evaluate("((T))", pf, answer) == 0 && pf == "T" && answer);
+    assert(evaluate("", pf, answer) == 1);
+    assert(evaluate("(sdf)", pf, answer) == 1);
+    assert(evaluate("()sdf", pf, answer) == 1);
+    assert(evaluate("!", pf, answer) == 1);
+    assert(evaluate("(", pf, answer) == 1);
+    assert(evaluate("!!!", pf, answer) == 1);
+    assert(evaluate("TF", pf, answer) == 1);
+    assert(evaluate("T(F", pf, answer) == 1);
+    assert(evaluate("T(F)", pf, answer) == 1);
+    assert(evaluate("()()", pf, answer) == 1);
+    assert(evaluate("&!", pf, answer) == 1);
+    assert(evaluate("^!", pf, answer) == 1);
+    assert(evaluate("&()", pf, answer) == 1);
+    assert(evaluate("!()", pf, answer) == 1);
+    assert(evaluate("F && F", pf, answer) == 1);
+    assert(evaluate("T", pf, answer) == 0);
+    assert(evaluate("!T", pf, answer) == 0);
+    assert(evaluate("(!(T))", pf, answer) == 0);
+    assert(evaluate("!(((T&F)))", pf, answer) == 0);
+    assert(evaluate("!T", pf, answer) == 0);
+    assert(evaluate("!( T & !F ^ T)", pf, answer) == 0);
+    assert(evaluate("T & !!F", pf, answer) == 0);
+    assert(evaluate("F", pf, answer) == 0);
+    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
+    assert(evaluate("(!(T))", pf, answer) == 0 && pf == "T!" && !answer);
+    assert(evaluate("!(((T&F)))", pf, answer) == 0 && pf == "TF&!" && answer);
+    assert(evaluate("!T", pf, answer) == 0 && pf == "T!" && !answer);
+    assert(evaluate("F", pf, answer) == 0 && pf == "F" && !answer);
+    assert(evaluate("!(T&!F) ^!T& !F", pf, answer) == 0 && pf == "TF!&!T!F!&^" && !answer);
+    assert(evaluate("!!!!T", pf, answer) == 0 && pf == "T!!!!" && answer);
+    assert(evaluate("!!T", pf, answer) == 0);
+    assert(evaluate("!!!!T", pf, answer) == 0);
+    assert(evaluate("!!(T) & !F ^ T", pf, answer) == 0);
+    assert(evaluate("!!!!T", pf, answer) == 0 && pf == "T!!!!" && answer);
+    assert(evaluate("T & !!F", pf, answer) == 0 && pf == "TF!!&" && !answer);
+    assert(evaluate("!( T & !F ^ T)", pf, answer) == 0 && pf == "TF!&T^!" && answer);
+    assert(evaluate("!!( T & !F ^ T)", pf, answer) == 0 && pf == "TF!&T^!!" && !answer);
+    assert(evaluate("!!!( T & !F ^ T)", pf, answer) == 0 && pf == "TF!&T^!!!" && answer);
+    assert(evaluate("!!(T) & !F ^ T", pf, answer) == 0 && pf == "T!!F!&T^" && !answer);
+    assert(evaluate("(T&T)(T&T)", pf, answer) == 1);
+    assert(evaluate("T!T", pf, answer) == 1);
+    assert(evaluate("f&t", pf, answer) == 1);
+    assert(evaluate("(((T&F)^((T)))", pf, answer) == 1);
+    cerr<<"Passed all test cases!"<<endl;
 }
-
